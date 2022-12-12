e77f982a85c590a1fb131c301daa7b9510d863f0
Franco Ortega <ortegafran97@gmail.com>
2022-12-12 15:48:03 -0300

    Eureka v0.1

--- residencies-service/src/main/java/com/donks/ResidenciesMgr/ResidenciesMgrApplication.java
+++ residencies-service/src/main/java/com/donks/ResidenciesMgr/ResidenciesMgrApplication.java
@@ -2,8 +2,10 @@ package com.donks.ResidenciesMgr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ResidenciesMgrApplication {
 
 	public static void main(String[] args) {
