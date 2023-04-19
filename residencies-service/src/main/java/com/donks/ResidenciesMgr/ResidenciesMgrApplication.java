3b0668c8e696b959b7b75788b2d34dbfa2d11da3
Franco Ortega <ortegafran97@gmail.com>
2023-04-19 17:12:15 -0300

    Feign client decorator

--- residencies-service/src/main/java/com/donks/ResidenciesMgr/ResidenciesMgrApplication.java
+++ residencies-service/src/main/java/com/donks/ResidenciesMgr/ResidenciesMgrApplication.java
@@ -3,9 +3,11 @@ package com.donks.ResidenciesMgr;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients
 public class ResidenciesMgrApplication {
 
 	public static void main(String[] args) {
